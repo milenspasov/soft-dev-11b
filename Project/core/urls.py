906f1cbdd0fd0d656affb6d3efe229582e6eccf9
Danail Yordanov <danail.yordanov1@gmail.com>
2022-03-01 17:00:22 +0200

    Clean up some things

--- Project/core/urls.py
+++ Project/core/urls.py
@@ -1,13 +1,6 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from django.contrib import admin
-from django.urls import path, include  # add this
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),          # Django admin route
-    path("", include("apps.authentication.urls")), # Auth routes - login / register
-    path("", include("apps.app.urls"))             # UI Kits Html files
+    path('admin/', admin.site.urls),
 ]
